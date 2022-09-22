0d10d67a709852b43b6b4a303cd406ec06f1a96d
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-22 19:50:34 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,sum=0;         n=sc.nextInt();         int tep=n;         for(int i=1;i<=n/2;i++)         {             if(n%i==0)             {                 sum=sum+i;
                }
              
            }
              if(sum==tep)
                System.out.println("True");
                else
                System.out.println("False");
            
            
        }
    }

---
+++ Perfect_Number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,sum=0;
+        n=sc.nextInt();
+        int tep=n;
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+                
+            }
+          
+        }
+          if(sum==tep)
+            System.out.println("True");
+            else
+            System.out.println("False");
+        
+        
+    }
+}
