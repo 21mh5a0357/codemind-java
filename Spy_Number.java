d25b3b15c8865dffaca709c809708c13a786d0fa
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-22 21:20:07 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,r,sum=0,prod=1;         n=sc.nextInt();         while(n>0)         {             r=n%10;             sum=sum+r;             prod=prod*r;             n=n/10;         }         if(sum==prod)         System.out.println("Spy Number");         else         System.out.println("Not Spy Number");     } }

--- Spy_Number.java
+++ Spy_Number.java
@@ -1,33 +1,21 @@
 import java.util.Scanner;
 class Main
 {
-    public static boolean spy(int n)
-    {
-        int prod=1,r,count=0;
-
-        while(n>0)
-        {
-            r=n%10;
-            prod=prod*r;
-            count=count+r;
-            n=n/10;
-            
-        }
-        if(count==prod)
-        return true;
-        else
-        return false;
-        
-        
-    }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        if(spy(n))
+        int n,r,sum=0,prod=1;
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+r;
+            prod=prod*r;
+            n=n/10;
+        }
+        if(sum==prod)
         System.out.println("Spy Number");
         else
         System.out.println("Not Spy Number");
-        
     }
 }
