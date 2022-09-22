d3928ed52e1fc0e1394d7425abf2f81efd5191f2
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-22 22:23:39 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m;         n=sc.nextInt();         m=sc.nextInt();         for(int i=n+1;i<m;i++)         {             if(prime(i))             System.out.println(i);         }
        }
    }

--- Generate_Prime_Numbers_In_A_Interval_.java
+++ Generate_Prime_Numbers_In_A_Interval_.java
@@ -3,28 +3,28 @@ class Main
 {
     public static boolean prime(int n)
     {
-        if(n==1)
-        return false;
+        int count=0;
         for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-            return false;
+            count++;
         }
+        if(count==0)
         return true;
+        else
+        return false;
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a,b;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        for(int i=a+1;i<b;i++)
+        int n,m;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        for(int i=n+1;i<m;i++)
         {
             if(prime(i))
             System.out.println(i);
         }
-        
-    
         
     }
 }
