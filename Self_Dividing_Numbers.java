d054bb16fb569764dcd0a64198221923749cc1b1
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-22 20:16:43 +0530

    import java.util.Scanner; class Main {     public static boolean self(int n)     {         int r;         int tep=n;         while(n>0)         {           r=n%10;           if(r!=0)           {           if(tep%r!=0)           {               return false;           }           }           else           return false;           n=n/10;         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m;         n=sc.nextInt();         m=sc.nextInt();         int tep=n;         for(int i=n;i<=m;i++)         {             if(self(i))             System.out.print(i+" ");         }
        }
    }

--- Self_Dividing_Numbers.java
+++ Self_Dividing_Numbers.java
@@ -4,34 +4,35 @@ class Main
     public static boolean self(int n)
     {
         int r;
-        int temp=n;
+        int tep=n;
         while(n>0)
         {
-            
-            r=n%10;
-            if(r!=0)
-            {
-            if(temp%r!=0)
-            return false;
-            }
-            else
+          r=n%10;
+          if(r!=0)
+          {
+          if(tep%r!=0)
+          {
               return false;
-            n=n/10;
-            
+          }
+          }
+          else
+          return false;
+          n=n/10;
         }
         return true;
-        
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a,b;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        for(int i=a;i<=b;i++)
+        int n,m;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        int tep=n;
+        for(int i=n;i<=m;i++)
         {
             if(self(i))
             System.out.print(i+" ");
         }
+    
     }
 }
