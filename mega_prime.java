172ab271668c05856eb27e9b34ca438371183c2a
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-24 01:14:20 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n=sc.nextInt(),count=0;
            int d=(int)Math.log10(n)+1;
            
            if(prime(n))
            {
            while(n>0)
            {
                int r=n%10;
                if(prime(r))
                count++;
                n=n/10;
            }
            }
            if(count==d)
            System.out.println("Mega Prime");
            else
            System.out.println("Not Mega Prime");
            
        }
    }

---
+++ mega_prime.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Main
+{
+    public static boolean prime(int n)
+    {
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),count=0;
+        int d=(int)Math.log10(n)+1;
+        
+        if(prime(n))
+        {
+        while(n>0)
+        {
+            int r=n%10;
+            if(prime(r))
+            count++;
+            n=n/10;
+        }
+        }
+        if(count==d)
+        System.out.println("Mega Prime");
+        else
+        System.out.println("Not Mega Prime");
+        
+    }
+}
