dbf4f25601ffdc5d9f6ee3f46b4185937c9dbf9c
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-24 00:25:00 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n=sc.nextInt();
            int d1,d2,i;
            for(i=n;;i++)
            {
            if(prime(i))
            {
            d1=i;
            break;
            }
            }
            for(i=n;;i--)
            {
            if(prime(i))
            {
            d2=i;
            break;
            }
            }
            if((d1-n)>(n-d2))
            System.out.println(n-d2);
            else
            System.out.println(d1-n);
        
        }
    }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Main
+{
+    public static boolean prime(int n)
+    {
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d1,d2,i;
+        for(i=n;;i++)
+        {
+        if(prime(i))
+        {
+        d1=i;
+        break;
+        }
+        }
+        for(i=n;;i--)
+        {
+        if(prime(i))
+        {
+        d2=i;
+        break;
+        }
+        }
+        if((d1-n)>(n-d2))
+        System.out.println(n-d2);
+        else
+        System.out.println(d1-n);
+    
+    }
+}
