116c370b0e01b384a0ddfb31889e860352dc58ed
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-09-24 00:32:39 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int t=sc.nextInt();
            int d1,d2,i;
            while(t>0)
            {
              int n=sc.nextInt();  
            for(i=n;;i++)
            {
            if(prime(i))
            {
            d1=i;
            break;
            }
            }
            for(i=n;;i--)
            {
            if(prime(i))
            {
            d2=i;
            break;
            }
            }
            
            if((d1-n)>(n-d2))
            System.out.println(d2);
            else if((d1-n)==(n-d2))
            System.out.println(d2);
            else
            System.out.println(d1);
            t--;
            }
            
        }
    }

--- Nearest_Prime.java
+++ Nearest_Prime.java
@@ -3,53 +3,49 @@ class Main
 {
     public static boolean prime(int n)
     {
-        int i,count=0;
-        for(i=2;i<=(int)Math.sqrt(n);i++)
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-            count++;
-            
+            return false;
         }
-        if(count==0)
         return true;
-        else
-        return false;
-        
-        
     }
+    
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,d1,d2;
         int t=sc.nextInt();
+        int d1,d2,i;
         while(t>0)
         {
-             n=sc.nextInt();
-             
-             for(int i=n;;i++)
-             {
-                 if(prime(i))
-                 {
-                 d1=i;
-                 break;
-                 }
-             }
-             for(int i=n;;i--)
-             {
-                if(prime(i))
-                {
-                d2=i;
-                break;
-                }
-             }
-             if((d1-n)>=(n-d2))
+          int n=sc.nextInt();  
+        for(i=n;;i++)
+        {
+        if(prime(i))
+        {
+        d1=i;
+        break;
+        }
+        }
+        for(i=n;;i--)
+        {
+        if(prime(i))
+        {
+        d2=i;
+        break;
+        }
+        }
+        
+        if((d1-n)>(n-d2))
+        System.out.println(d2);
+        else if((d1-n)==(n-d2))
         System.out.println(d2);
         else
         System.out.println(d1);
-             t--;
+        t--;
         }
-        
-    
         
     }
 }
