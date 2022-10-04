61a0bfe80a9b461e40e85c50fb2a930e3ef21c51
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-04 08:21:38 +0530

    import java.util.Scanner; class Main {     public static int rev(int n)     {         int r=0;         while(n!=0)         {          r=r*10+n%10;          n=n/10;         }         return r;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int max=rev(n);
          System.out.println(max);
         
        }
        
    }

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,16 +1,25 @@
-import java.util.*;
-class Sample
+import java.util.Scanner;
+class Main
 {
+    public static int rev(int n)
+    {
+        int r=0;
+        while(n!=0)
+        {
+         r=r*10+n%10;
+         n=n/10;
+        }
+        return r;
+    }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int rev=0;
-        while(n!=0)
-        {
-        rev=rev*10+n%10;
-        n=n/10;
-        }
-        System.out.println(rev);
+        int n;
+        n=sc.nextInt();
+        int max=rev(n);
+      
+      System.out.println(max);
+     
     }
+    
 }
