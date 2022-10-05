9e5e58c57ce4af0829113a114b4da5ea4d4702d4
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-05 15:03:42 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n=sc.nextInt(),d=0;         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]%2==1)             {                 System.out.print(x[i]+" ");
                }
        
            }
                    for(int i=0;i<n;i++)
            { 
                if(x[i]%2==0)
                {
                    System.out.print(x[i]+" ");
                 
                }
      
            }
       }  
    }

--- array_of_add_and_even_elements.java
+++ array_of_add_and_even_elements.java
@@ -4,25 +4,27 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],i,n;
-        n=sc.nextInt();
+        int x[],n=sc.nextInt(),d=0;
         x=new int[n];
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         x[i]=sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-           if(x[i]%2!=0)
-           {
-           System.out.print(x[i]+" ");
-           }
+        for(int i=0;i<n;i++)
+        { 
+            if(x[i]%2==1)
+            {
+                System.out.print(x[i]+" ");
+             
+            }
+    
         }
-        for(i=0;i<n;i++)
-        {
-           if(x[i]%2==0)
-           {
-           System.out.print(x[i]+" ");
-           }
+                for(int i=0;i<n;i++)
+        { 
+            if(x[i]%2==0)
+            {
+                System.out.print(x[i]+" ");
+             
+            }
+  
         }
-        
-    }
+   }  
 }
