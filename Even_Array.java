05f796626b7bacb9c9d7010394983745faa3be06
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-05 16:40:31 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],i,n,a,b,count=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();
            for(i=0;i<n;i++)
            {
               if(x[i]%2!=0)
               {
                 count=1;
               }
            }
            if(count==0)
            System.out.print("True");
            else
            System.out.println("False");
            
        }
    }

--- Even_Array.java
+++ Even_Array.java
@@ -4,19 +4,23 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],i,n,sum=0,count=0;
+        int x[],i,n,a,b,count=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         x[i]=sc.nextInt();
+       
         for(i=0;i<n;i++)
         {
-            if(x[i]%2==0)
-            count++;
+           if(x[i]%2!=0)
+           {
+             count=1;
+           }
         }
-        if(count==n)
-        System.out.println("True");
+        if(count==0)
+        System.out.print("True");
         else
         System.out.println("False");
+        
     }
 }
