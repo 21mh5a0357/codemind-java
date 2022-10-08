ad635055af79012598a350cb73e458416ee06ceb
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-08 12:26:39 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,sum=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();          for(int i=0;i<n;i++)          {              if(x[i]>=a && x[i]<=b)              sum=sum+x[i];          }           System.out.println(sum);     } }

--- sum_of_elements_between_A_and_B.java
+++ sum_of_elements_between_A_and_B.java
@@ -4,20 +4,18 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],i,a,b,n,sum=0;
+        int x[],n,sum=0;
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         x[i]=sc.nextInt();
-        a=sc.nextInt();
-        b=sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-            if(x[i]>=a && x[i]<=b)
-            {
-                sum=sum+x[i];
-            }
-        }
-        System.out.println(sum);
-        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+         for(int i=0;i<n;i++)
+         {
+             if(x[i]>=a && x[i]<=b)
+             sum=sum+x[i];
+         }
+          System.out.println(sum);
     }
+}
