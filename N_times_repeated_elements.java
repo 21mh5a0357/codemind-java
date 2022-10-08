3f91a84c148b887661fbae1aeb78d569d572852c
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-08 14:33:26 +0530

    import java.util.Scanner; class Frequency {
       public static void main(String[] args) {
            // TODO Auto-generated method stub
            int n,i,j,x[],count=0,flag=0;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
                x[i]=sc.nextInt();
            int k=sc.nextInt();
            for(i=0;i<n;i++)
            {    count=1;
                if(x[i]!=-99)
                {
                  for(j=0;j<n;j++)
                  {
                    if(x[i]==x[j] && i!=j)
                    {
                        count++;
                        x[j]=-99;
                    }
                  }
                  //System.out.println(x[i]+" "+count);
                  if(k==count)
                    {
                     System.out.print(x[i]+" ");
                      flag=1;
                    }
                }
            }
            if(flag==0)
               System.out.println("-1");
    
    
    
       }
    
    
    
    }

--- N_times_repeated_elements.java
+++ N_times_repeated_elements.java
@@ -1,40 +1,44 @@
-
 import java.util.Scanner;
-class Sanmple 
-{
+class Frequency {
 
- public static void main(String[] args) 
-{
-// TODO Auto-generated method stu
-int n,i,j,x[],count=0,flag=0;
-Scanner sc=new Scanner(System.in);
-n=sc.nextInt();
-x=new int[n];
-for(i=0;i<n;i++)
-x[i]=sc.nextInt();
-int k=sc.nextInt();
-for(i=0;i<n;i++)
-{
-count=1;
-if(x[i]!=-99)
-{
-for(j=0;j<n;j++)
-{
-if(x[i]==x[j] && i!=j)
-{
-count++;
-x[j]=-99;
-}
-}
-}
-if(k==count)
-{
-System.out.print(x[i]+" ");
-flag++;
-}
 
-}
-if(flag==0)
-System.out.println("-1");
-}
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,i,j,x[],count=0,flag=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        int k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {    count=1;
+            if(x[i]!=-99)
+            {
+              for(j=0;j<n;j++)
+              {
+                if(x[i]==x[j] && i!=j)
+                {
+                    count++;
+                    x[j]=-99;
+                }
+              }
+              //System.out.println(x[i]+" "+count);
+              if(k==count)
+                {
+                 System.out.print(x[i]+" ");
+                  flag=1;
+                }
+            }
+        }
+        if(flag==0)
+           System.out.println("-1");
+
+
+
+   }
+
+
+
 }
