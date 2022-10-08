3fcd448329ba5a7baf505387e098ba9a1ac5210a
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-08 07:27:17 +0530

    import java.util.Scanner; class Sanmple {
     public static void main(String[] args) 
    {
    // TODO Auto-generated method stu
    int n,i,j,x[],count=0,flag=0;
    Scanner sc=new Scanner(System.in);
    n=sc.nextInt();
    x=new int[n];
    for(i=0;i<n;i++)
    x[i]=sc.nextInt();
    for(i=0;i<n;i++)
    {
    count=1;
    if(x[i]!=-99)
    {
    for(j=0;j<n;j++)
    {
    if(x[i]==x[j] && i!=j)
    {
    count++;
    x[j]=-99;
    }
    }
    }
    if(x[i]==count)
    {
    flag++;
    }
    
    }
    System.out.println(flag);
    }
    }

---
+++ value_same_as_count.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+class Sanmple 
+{
+
+ public static void main(String[] args) 
+{
+// TODO Auto-generated method stu
+int n,i,j,x[],count=0,flag=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+x=new int[n];
+for(i=0;i<n;i++)
+x[i]=sc.nextInt();
+for(i=0;i<n;i++)
+{
+count=1;
+if(x[i]!=-99)
+{
+for(j=0;j<n;j++)
+{
+if(x[i]==x[j] && i!=j)
+{
+count++;
+x[j]=-99;
+}
+}
+}
+if(x[i]==count)
+{
+flag++;
+}
+
+}
+System.out.println(flag);
+}
+}
