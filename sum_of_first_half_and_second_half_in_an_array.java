728689a22319c4fdd38bd05db5ce134ce0f409f2
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-11 23:15:17 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],i,n,sum=0,add=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<(int)n/2;i++)         {
                sum=sum+x[i];
            }
            System.out.println(sum);
             for(i=(int)n/2;i<n;i++)
            {
                add=add+x[i];
            }
            System.out.println(add);
        }
    }

---
+++ sum_of_first_half_and_second_half_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],i,n,sum=0,add=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<(int)n/2;i++)
+        {
+            
+            sum=sum+x[i];
+        }
+        System.out.println(sum);
+         for(i=(int)n/2;i<n;i++)
+        {
+            add=add+x[i];
+        }
+        System.out.println(add);
+    }
+}
