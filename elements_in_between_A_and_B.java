08c9060e27727d9b3d59d0e12a48d78cd3ab2993
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-11 20:39:21 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],a,b,found=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]>=a && x[i]<=b)             {                 System.out.print(x[i]+" ");                 found=1;             }         }         if(found==0)         System.out.println(-1);     } }

--- elements_in_between_A_and_B.java
+++ elements_in_between_A_and_B.java
@@ -4,24 +4,24 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],i,a,b,n,count=0;
+        int n,x[],a,b,found=0;
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
-        x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
         a=sc.nextInt();
         b=sc.nextInt();
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             if(x[i]>=a && x[i]<=b)
             {
-               System.out.print(x[i]+" ");
-               count++;
+                System.out.print(x[i]+" ");
+                found=1;
             }
-            
         }
-        if(count==0)
-            System.out.println("-1");
-        }
+        if(found==0)
+        System.out.println(-1);
     }
- 
+}
