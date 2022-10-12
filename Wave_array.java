ef35d58170a4ed6e9ecd1cf774c7ce0639d9f104
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-10-12 11:54:27 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,count=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         for(int i=1;i<n-1;i++)         {             if(x[i-1]<x[i] && x[i]>x[i+1])             count++;             else if(x[i-1]>x[i] && x[i]<x[i+1])             count++;         }         if(count==n-2)         System.out.println("yes");         else         System.out.println("no");     } }

--- Wave_array.java
+++ Wave_array.java
@@ -4,27 +4,21 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],n,c=0;
+        int x[],n,count=0;
         n=sc.nextInt();
         x=new int[n];
         for(int i=0;i<n;i++)
         x[i]=sc.nextInt();
         for(int i=1;i<n-1;i++)
         {
-            if(x[i+1]>x[i] && x[i-1]>x[i])
-            c++;
-            else if(x[i+1]<x[i] && x[i-1]<x[i])
-            {
-                c++;
-            }
+            if(x[i-1]<x[i] && x[i]>x[i+1])
+            count++;
+            else if(x[i-1]>x[i] && x[i]<x[i+1])
+            count++;
         }
-        if(c==n-2)
-        {
-            System.out.println("yes");
-        }
+        if(count==n-2)
+        System.out.println("yes");
         else
-        {
-            System.out.println("no");
-        }
+        System.out.println("no");
     }
 }
