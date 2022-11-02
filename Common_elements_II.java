6c5b16dd60a7f36472add5299d6280744a9a0e26
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-11-02 23:17:45 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m,x[],y[],i,c,j,z[],d=0;         n=sc.nextInt();         m=sc.nextInt();         x=new int[n];         y=new int[m];         z=new int[n+m];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<m;i++)         {             y[i]=sc.nextInt();         }         sc.close();         for(i=0;i<n;i++)         {             c=0;             for(j=0;j<m;j++)             {                 if(x[i]==y[j])                 {                     c=1;                 }             }             if(c==0)             {                 z[d]=x[i];                 d++;             }         }         for(i=0;i<m;i++)         {             c=0;             for(j=0;j<n;j++)             {                 if(y[i]==x[j])                 {                     c=1;                 }             }             if(c==0)             {                 z[d]=y[i];                 d++;             }         }         for(i=0;i<d;i++)         {             System.out.print(z[i]+" ");         }     } }

--- Common_elements_II.java
+++ Common_elements_II.java
@@ -1,38 +1,59 @@
-import java.util.*;
- class array{
-     public static void main(String[] args){
-         Scanner sc=new Scanner(System.in);
-         int n=sc.nextInt(),count=0;
-         int m=sc.nextInt();
-         int x[]=new int[n];
-         int y[]=new int[m];
-         for(int i=0;i<n;i++)
-             x[i]=sc.nextInt();
-         for(int i=0;i<m;i++)
-             y[i]=sc.nextInt();
-        for(int i=0;i<n;i++){
-            count=0;
-            for(int j=0;j<m;j++){
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,m,x[],y[],i,c,j,z[],d=0;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        x=new int[n];
+        y=new int[m];
+        z=new int[n+m];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<m;i++)
+        {
+            y[i]=sc.nextInt();
+        }
+        sc.close();
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            for(j=0;j<m;j++)
+            {
                 if(x[i]==y[j])
                 {
-                  count++;
+                    c=1;
                 }
-                }
-                if(count==0)
-                System.out.print(x[i]+" ");
-            
-            }   
-        for(int i=0;i<m;i++){
-            count=0;
-            for(int j=0;j<n;j++){
-                if(x[j]==y[i])
+            }
+            if(c==0)
+            {
+                z[d]=x[i];
+                d++;
+            }
+        }
+        for(i=0;i<m;i++)
+        {
+            c=0;
+            for(j=0;j<n;j++)
+            {
+                if(y[i]==x[j])
                 {
-                  count++;
+                    c=1;
                 }
-                }
-                if(count==0)
-                System.out.print(y[i]+" ");
-            
-            }   
+            }
+            if(c==0)
+            {
+                z[d]=y[i];
+                d++;
+            }
+        }
+        for(i=0;i<d;i++)
+        {
+            System.out.print(z[i]+" ");
+        }
     }
- }
+}
