d2f21fc0c63a358d644c2cfebc23ee07899b4135
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-11-09 16:06:01 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),x[],k,found=0;         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]==k){             System.out.print(i+" ");             found=1;             break;             }
            }
            if(found==0)
            {
                System.out.print(-1);
            }
        }
    }

---
+++ Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),x[],k,found=0;
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]==k){
+            System.out.print(i+" ");
+            found=1;
+            break;
+            }
+           
+            
+        }
+        if(found==0)
+        {
+            System.out.print(-1);
+        }
+    }
+}
