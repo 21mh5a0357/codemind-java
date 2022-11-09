c7dae2a7a19bb176952ef3d068b902272844d612
21mh5a0357 <112751602+21mh5a0357@users.noreply.github.com>
2022-11-09 15:56:06 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),x[],k,found=0;         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]==k && found==0){             System.out.print(i+" ");             found=1;             }             if(x[i]>k && found==1)             {                 System.out.print(i-1);                 break;             }
            }
            if(found==0)
            {
                System.out.print(-1+" "+-1);
            }
        }
    }

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),x[],k,found=0;
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]==k && found==0){
+            System.out.print(i+" ");
+            found=1;
+            }
+            if(x[i]>k && found==1)
+            {
+                System.out.print(i-1);
+                break;
+            }
+            
+        }
+        if(found==0)
+        {
+            System.out.print(-1+" "+-1);
+        }
+    }
+}
